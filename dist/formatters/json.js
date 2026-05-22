9efc4ae2fe8ffe91f99dbde44aebcf7e8e47dfe0
axel valles <agabrielv1799@gmail.com>
2026-05-22 14:18:25 -0500

    chore: gitignore dist/ build artifacts

--- dist/formatters/json.js
+++
@@ -1,6 +0,0 @@
-export function formatScanJson(envelope) {
-    return JSON.stringify(envelope, null, 2);
-}
-export function formatInstallJson(envelope) {
-    return JSON.stringify(envelope, null, 2);
-}
